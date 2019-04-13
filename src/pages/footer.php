a8ede5263cef04d5f775fd592676282db449e6a0
bilcker <47618203+bilcker@users.noreply.github.com>
2019-04-13 10:33:41 -0400

    Theme Setup
    Added a very basic header and footer php. Also include the main js and style sheet into the functions file

---
+++ src/pages/footer.php
@@ -0,0 +1,6 @@
+    <footer>
+      <p>I am a testing footer</p>
+    </footer>
+    <?php wp_footer(); ?>
+  </body>
+</html>
--- src/pages/header.php
+++ src/pages/header.php
@@ -1,15 +1,15 @@
 <!doctype html>
-    <head>
-        <!--=== META TAGS ===-->
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <meta charset="<?php bloginfo( 'charset' ); ?>" />
-        <meta name="description" content="Keywords">
-        <meta name="author" content="Name">
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <head>
+    <!--=== META TAGS ===-->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta name="description" content="Keywords">
+    <meta name="author" content="Name">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
      
     <!--=== LINK TAGS ===-->
     <link rel="shortcut icon" href="<?php echo THEME_DIR; ?>/path/favicon.ico" />
-    <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS2 Feed" href="<?php bloginfo('rss2_url'); ?>" />
+    <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS2 Feed" href="<?php bloginfo('rss2_url'); ?>" />dock
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
  
     <!--=== TITLE ===-->  
@@ -20,4 +20,8 @@
     </head>
 </html>
 
-<body <?php body_class(); ?>
+<body <?php body_class(); ?>>
+
+  <header>
+    <h1>I am the test Header</h1>
+  </header>
